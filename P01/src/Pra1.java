907919b746d2229975199cd1f2a1bae3cfabcd51
LinDragon1002 <lindragon1002@gmail.com>
2025-10-31 17:35:05 +0800

    java程式設計上課練習

--- P01/src/Pra1.java
+++ P01/src/Pra1.java
@@ -6,8 +6,27 @@ public class Pra1 {
         Integer n1 = sc.nextInt();
         Integer n2 = sc.nextInt();
         Integer n3 = sc.nextInt();
-        Integer maxn = Math.max(Math.max(n1,n2),n3);
-        Integer minn = Math.min(Math.min(n1,n2),n3);
-        
+        if(n1 > n2 && n1 > n3) {
+            if (n2 > n3) {
+                System.out.println(n1+">"+n2+">"+n3);
+            }else {
+                System.out.print(n1+">"+n3+">"+n2);
+            }
+            
+        } else if (n2 > n1 && n2 > n3) {
+            if (n1 > n3){
+               System.out.println(n2+">"+n1+">"+n3); 
+            }else {
+                System.out.println(n2+">"+n3+">"+n1);
+            }
+            
+        } else if (n3 > n1 && n3 > n2) {
+            if (n1 > n2) {
+                System.out.println(n3+">"+n1+">"+n2);
+            } else {
+                System.out.println(n3+">"+n2+">"+n1);
+            }
+        }
+                
     }
 }
