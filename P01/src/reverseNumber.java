adcff774c4f073a207023026dab1cc65efbc1f1a
LinDragon1002 <lindragon1002@gmail.com>
2025-11-06 21:53:14 +0800

    java程式設計期中考前練習

--- P01/src/reverseNumber.java
+++ P01/src/reverseNumber.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class reverseNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer number = sc.nextInt();
-        String numbeString = Integer.toString(number);
-        Integer run = 1;
-        String ans = "";
-        if (number < 0) {
-            System.out.println();
-        } else {
-            while (numbeString.length() >= run) {
-                ans += numbeString.charAt(numbeString.length()-run);
-                run++;
+
+        while (true) {
+            int num = sc.nextInt();
+            if (num < 0) {
+                break;
             }
-            System.out.println(Integer.parseInt(ans));
+
+            int ans = 0;
+            while (num > 0){
+                ans = ans * 10 + num % 10;
+                num /= 10;
+            }
+            System.out.println(ans);
         }
     }
-    
 }
